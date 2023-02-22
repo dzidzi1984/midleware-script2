52769e6f1fd5f256fdaa68ae72dd4fce41cf98c6
komla <romgbet@gmail.com>
2023-02-21 21:41:36 -0600

    sona install

--- sonarqube.sh
+++ sonarqube.sh
@@ -15,9 +15,9 @@ sudo yum install wget -y
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip01
 sudo unzip /opt/sonarqube-9.3.0.51899.zip01
 sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
-cd sonarqube-9.3.051899/
-cd bin/
-cd linux-x86-64/ 
+cd sonarqube-9.3.051899
+cd bin
+cd linux-x86-64
  ./sonar.sh start
  echo "Sonarqube installed and started successfully"
 
